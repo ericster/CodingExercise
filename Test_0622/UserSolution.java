9a168906ac65acdaf9b7f162411fe5e8ecb08ced
ericpoof <ericpoof@gmail.com>
2018-06-26 23:15:05 -0700

    2nd try - answer debug and TLE

--- Test_0622/UserSolution.java
+++ Test_0622/UserSolution.java
@@ -1,9 +1,12 @@
-class UserSolution {
+import java.util.Arrays;
+
+public class UserSolution {
 
     int piece_no;
     int N, M, K;
     int [][] pieces;
     int [] index; 
+    int [] res;
 
     public void init(int n, int m, int k)
     {
@@ -13,6 +16,7 @@ class UserSolution {
         K = k;
         pieces = new int[n*n+k][4*m];
         index = new int[n*n];
+        res = new int[]{-1};
 
 /*
     0: 1 2 3 4
@@ -37,6 +41,7 @@ class UserSolution {
         for (int i=0;i<4*M;i++){
             pieces[piece_no][i] = piece[i];
         }
+        System.out.println("no " + piece_no + Arrays.toString(pieces[piece_no]));
         piece_no++;
         
     }
@@ -68,10 +73,22 @@ class UserSolution {
         // permutation dfs
 
         int[] arr = new int[N*N+K];
-        for (int i=0;i<N*N+K;i++){
+        int _N = N*N+K;
+        int _K = N*N;
+        //_N = 4;
+        //_K = 3;
+        for (int i=0;i<_N;i++){
             arr[i] = i;
         }
-        genKPermutation(arr,N,K); 
+            int[] ans = new int[]{ 1, 8, 3, 4, 2, 5, 0, 9, 6};
+            //if (isRightPieces(arr)){
+            if (isRightPieces(ans)){
+                int center_idx = arr[(N-1)/2];
+                int[] center = pieces[center_idx];
+                System.out.println("center " + center_idx);
+                System.out.println("center arr" + Arrays.toString(center));
+            }
+        //genKPermutation(arr,_N, _K); 
 
 
         for (int i=0;i<N*N;i++){
@@ -81,41 +98,111 @@ class UserSolution {
     }
 
 
-    // [0,1,3,4...8]
+    /* 
+       1 8 3 
+       4 2 5
+       0 9 6
+    
+       [0 | 1 | 2   ; 0/1:1/3, 1/1:2/3,   odd , left:1, right:2  
+        -   -   -   ; 0/2:3/0, 1/2:4/0, 2/2:5/0  even
+        3 | 4 | 5   ; 3/1:4/3, 4/1:5/3, odd
+        -   -   -   ; 3/2:6/0, 4/2:7/0, 5/2:8/0 even
+        6 | 7 | 8 ] ; 6/1:7/3, 7/1:8/3  odd  
+
+
+        (N-1)*N
+        0(0,1), 1(1,2) ,2(3,4) 3(4,5), 4(6,7), 5(7,8)
+
+     */
     public boolean isRightPieces(int[] pieceArr){
         /*
          index = new int[n*n]
          visited = new int[n*n]
          neighbor:     [-1,1,-m,m]
          */
+        System.out.println("peieceArr " + Arrays.toString(pieceArr));
         int[] neighbor = new int[]{-1*M,1,M, -1};
 
-        for (int i=0;i<N*N;i++){
-            int[] curr = pieces[i];
-            for (int j=0;j<4;i++){
-                // 0:up, 1:right, 2:down, 3:left
-                if ((i+neighbor[j]) >=0 && (i+neighbor[j]) < N){
-                    int[] neigh = pieces[i+neighbor[j]];
-                    for (int k=0;k<M;k++){
-                        if (curr[k] != neigh[k]){
-                            return false;
-                        }
+        /* between sides
+        (N-1)*N
+        0(0,1), 1(1,2),
+        2(3,4) 3(4,5), 
+        4(6,7), 5(7,8)
+        pieces = new int[n*n+k][4*m];
+        */
+        int[] left, right;
+        int[] left_1 = new int[M];
+        int[] right_3 = new int[M];
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N-1;j++){
+                // 0~M-1, M~2M-1, 2*M~3M-1, 3M~4M-1 
+                left = pieces[pieceArr[j+i]]; // left[1]  M~2M-1, 
+                right = pieces[pieceArr[j+1+i]]; // right[3] 3M~4M-1
+                System.out.println("left " + pieceArr[j+i]); // left[1]  M~2M-1, 
+                System.out.println("left arr" + Arrays.toString(left));
+                System.out.println("right " + pieceArr[j+1+i]); // left[1]  M~2M-1, 
+                System.out.println("right arr" + Arrays.toString(right));
+                for (int k=0;k<M;k++){
+                    left_1[k]=left[k+M];
+                    right_3[k]= right[k+3*M];
+                }
+                System.out.println("left_1 " + Arrays.toString(left_1));
+                System.out.println("right_3 " + Arrays.toString(right_3));
 
+                for (int k=0;k<M;k++){
+                    if (left_1[k] != right_3[k]){
+                        return false;
+                    }
+
+                }
+            }
+        }
+        /* between up/down
+        (N)*(N-1)
+        -   -   -   ; 0/2:3/0, 1/2:4/0, 2/2:5/0  even
+        -   -   -   ; 3/2:6/0, 4/2:7/0, 5/2:8/0 even
+        0(0,3), 1(1,4), 2(2,5)
+        3(3,6), 4(4,7), 5(5,8)
+        pieces = new int[n*n+k][4*m];
+        */
+        int[] up, dn;
+        int[] up_0 = new int[M];
+        int[] dn_2 = new int[M];
+        for (int i=0;i<N-1;i++){
+            for (int j=0;j<N;j++){
+                up = pieces[pieceArr[j]]; // up[0]  M~2M-1, 
+                dn = pieces[pieceArr[j+i*M]]; // dn[2] 3M~4M-1
+                System.out.println("up " + pieceArr[j]); // left[1]  M~2M-1, 
+                System.out.println("dn " + pieceArr[j+i*M]); // left[1]  M~2M-1, 
+                for (int k=0;k<M;k++){
+                    up_0[k]= up[k];
+                    dn_2[k]= dn[k+2*M];
+                }
+
+                for (int k=0;k<M;k++){
+                    if (up_0[k] != dn_2[k]){
+                        return false;
                     }
 
                 }
 
             }
+
         }
 
+
         return true;
+    }
+
+    public void genKPermutation(int arr[], int n, int r) {
+        int data[]= new int[r];
+        combinationUtil(arr, n, r, 0, data, 0);
     }
 
     public void combinationUtil(int arr[], int n, int r, int index, int data[], int i) {
         if (index == r)
         {
-            int len = data.length;
-            permute(data,0,len-1);
+            permute(data,0,r-1);
             // data is fixed
             return;
         }
@@ -133,23 +220,26 @@ class UserSolution {
         combinationUtil(arr, n, r, index, data, i+1);
     }
 
-    public void genKPermutation(int arr[], int n, int r) {
-        int data[]= new int[r];
-        combinationUtil(arr, n, r, 0, data, 0);
-    }
 
     public void permute(int[] arr, int l, int r)
     {
         if (l == r) {
-            if (isRightPieces(arr)){
-                int center_idx = 0;
+            System.out.println("peieceArr " + Arrays.toString(arr));
+            
+            int[] ans = new int[]{ 1, 8, 3, 4, 2, 5, 0, 9, 6};
+            //if (isRightPieces(arr)){
+            if (isRightPieces(ans)){
+                int center_idx = arr[(N-1)/2];
                 int[] center = pieces[center_idx];
+                System.out.println("center " + center_idx);
+                System.out.println("center arr" + Arrays.toString(center));
 
                 //TODO: to calculate the number of '1'
                 // ?? : return how?
                 return ;
             }
-            return;
+            //System.out.println(Arrays.toString(arr));
+            //return;
         }
         else
         {
