7a5a9ca40efafe1c467b2306fc68c44411710572
ericpoof <ericpoof@gmail.com>
2018-07-12 00:48:08 -0700

    TextEditorIV: add Solutoin, UserSolution, sample_input.txt

---
+++ Text_editor_4_0224_18/UserSolution.java
@@ -0,0 +1,32 @@
+public class UserSolution {
+    public void init(int N, char str[]){
+
+    }
+
+    public int insert_string( int M, char str[]){
+
+        return 1;
+    }
+
+    public void select_string(int mLeft, int mRight){
+
+    }
+
+    public void copy_string(){
+
+    }
+
+    public int cut_string(){
+
+        return 1;
+    }
+
+    public int paste_string(){
+        return 1;
+    }
+
+    public void get_substring(int mLeft, int mRight, char res[]){
+
+    }
+
+}
