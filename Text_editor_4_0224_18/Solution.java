20ec57873a53e3ed94f831fd793be8e2ee13e780
Moonhyun Eric Jung <me.jung@partner.sea.samsung.com>
2018-07-11 18:31:07 -0700

    TextEditorIV: add Solution.java

---
+++ Text_editor_4_0224_18/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Solution{
+
+
+    static final int SCP = 0;
+    static final int SCU = 1;
+    static final int SIN = 2;
+    static final int SPA = 3;
+    static final int PAS = 4;
+    static final int INS = 5;
+    static final int GET = 6;
+    static final int LEN = 7;
+    static final int INSERT = 100;
+    static final int SELECT = 200;
+    static final int COPY = 300;
+    static final int CUT = 400;
+    static final int PASTED = 500;
+    static final int GETSUBSTRING = 999;
+
+    static char init_str[] = new char[100005];
+    static char insert_str[] = new char[1005];
+    static char res_str[] = new char[15];
+
+
+
+
+
+
+
+}
